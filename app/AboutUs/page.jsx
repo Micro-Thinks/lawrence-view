5f9a6c02b6ef2ea62edbc40e3154ae9827e01664
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-10-30 11:58:13 +0500

    footer placement fixed

---
+++ app/AboutUs/page.jsx
@@ -0,0 +1,14 @@
+function page() {
+  return (
+    <div className="">
+      <div className="flex justify-center items-center p-auto mt-6 flex-col">
+        <h1 className="text-3xl">About</h1>
+        <h2 className="text-2xl font-bold mt-4">
+          Luxury Accommodation in Lahore
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default page;
